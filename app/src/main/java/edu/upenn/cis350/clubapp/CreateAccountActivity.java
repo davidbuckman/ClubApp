6b21c2e86815cdcb38d1e45d24b606526d8c3924
davidbuckman <davidbuckman7@gmail.com>
2017-02-14 16:19:26 -0500

    Read input from view

--- app/src/main/java/edu/upenn/cis350/clubapp/CreateAccountActivity.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/CreateAccountActivity.java
@@ -3,6 +3,7 @@ package edu.upenn.cis350.clubapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by david on 2/14/2017.
@@ -10,14 +11,26 @@ import android.view.View;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mConfirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+        mFirstName       = (EditText) findViewById(R.id.editFirstName);
+        mLastName        = (EditText) findViewById(R.id.editLastName);
+        mEmail           = (EditText) findViewById(R.id.editEmail);
+        mPassword        = (EditText) findViewById(R.id.editPassword);
+        mConfirmPassword = (EditText) findViewById(R.id.editConfirmPassword);
     }
 
     public void signUp(View view) {
-        System.out.println("clicked");
+        System.out.println(mFirstName.getText().toString());
     }
 
 }
