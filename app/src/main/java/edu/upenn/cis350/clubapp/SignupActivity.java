46c77d309d7e69477f3e7b29a0d94f4591b14a86
Abhi <suriabhinav1997@gmail.com>
2017-02-17 22:03:43 -0500

    init advanced login

--- app/src/main/java/edu/upenn/cis350/clubapp/SignupActivity.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/SignupActivity.java
@@ -3,11 +3,11 @@ package edu.upenn.cis350.clubapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 }
