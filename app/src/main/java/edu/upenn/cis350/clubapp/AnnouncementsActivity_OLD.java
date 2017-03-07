54546acebbfa04dc11dbae9407db4927bd4db818
davidbuckman <davidbuckman7@gmail.com>
2017-03-07 15:21:21 -0500

    Pullout drawer with user information

--- app/src/main/java/edu/upenn/cis350/clubapp/AnnouncementsActivity_OLD.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/AnnouncementsActivity_OLD.java
@@ -1,22 +1,17 @@
 package edu.upenn.cis350.clubapp;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,8 +26,12 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class AnnouncementsActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class AnnouncementsActivity_OLD extends AppCompatActivity {
+
+    // Navigation drawer resources
+    private String[] mNavigationOptions;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -51,46 +50,21 @@ public class AnnouncementsActivity extends AppCompatActivity
 
     String clubName;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_announcements);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_announcements_old);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        // Set navigation header information to current user
-        final TextView navigationHeaderName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.nav_header_name);
-        final TextView navigationHeaderEmail = (TextView) navigationView.getHeaderView(0).findViewById(R.id.nav_header_email);
-        mDatabaseReference.child("users").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                navigationHeaderName.setText(dataSnapshot.child("firstName").getValue() + " " + dataSnapshot.child("lastName").getValue());
-                navigationHeaderEmail.setText(dataSnapshot.child("email").getValue().toString());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-//        navigationHeaderEmail.setText(auth.getCurrentUser().getEmail());
+        // Initialize navigation drawer resources
+        mNavigationOptions = getResources().getStringArray(R.array.navigation_options);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_announcements);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mNavigationOptions));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         // Initialize data structures
         clubChannels = new HashSet<String>();
@@ -107,7 +81,7 @@ public class AnnouncementsActivity extends AppCompatActivity
             startActivity(i);
         }
 
-        //set up for display
+         //set up for display
         mRecyclerView = (RecyclerView) findViewById(R.id.notification_recycler_view);
 
         if (mRecyclerView != null) {
@@ -118,6 +92,7 @@ public class AnnouncementsActivity extends AppCompatActivity
         //using staggered grid pattern in recyclerview
         mLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mLayoutManager);
+
 
         //get data and display
         DatabaseReference ref = mDatabaseReference;
@@ -156,7 +131,7 @@ public class AnnouncementsActivity extends AppCompatActivity
                 final Iterator<String> myIter = myChannels.iterator();
                 System.out.println("we have iter? " + myIter.hasNext());
                 if(!myIter.hasNext()){
-                    Toast.makeText(AnnouncementsActivity.this,
+                    Toast.makeText(AnnouncementsActivity_OLD.this,
                             "You have no new messages!",
                             Toast.LENGTH_LONG).show();
                 }
@@ -184,40 +159,41 @@ public class AnnouncementsActivity extends AppCompatActivity
 
             }
         });
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+    // Navigation drawer item click listener
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+        /** Opens selected activity */
+        private void selectItem(int position) {
+            Intent i = new Intent();
+            i.putExtra("CLUB", clubName);
+            String selection = mNavigationOptions[position];
+            if (selection.equalsIgnoreCase("information")) {
+                i.setClass(AnnouncementsActivity_OLD.this, AnnouncementsActivity_OLD.class);
+            } else if (selection.equalsIgnoreCase("announcements")) {
+                i.setClass(AnnouncementsActivity_OLD.this, AnnouncementsActivity_OLD.class);
+            } else if (selection.equalsIgnoreCase("calendar")) {
+                i.setClass(AnnouncementsActivity_OLD.this, AnnouncementsActivity_OLD.class);
+            } else if (selection.equalsIgnoreCase("directory")) {
+                i.setClass(AnnouncementsActivity_OLD.this, AnnouncementsActivity_OLD.class);
+            } else if (selection.equalsIgnoreCase("club settings")) {
+                i.setClass(AnnouncementsActivity_OLD.this, ClubSettingsActivity.class);
+            }
+
+            // Highlight the selected item, update the title, and close the drawer
+            mDrawerList.setItemChecked(position, true);
+            mDrawerLayout.closeDrawer(mDrawerList);
+            startActivity(i);
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_information) {
-            // Handle the camera action
-        } else if (id == R.id.nav_announcements) {
-
-        } else if (id == R.id.nav_calendar) {
-
-        } else if (id == R.id.nav_directory) {
-
-        } else if (id == R.id.nav_club_settings) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
     public class RVAdapter extends RecyclerView.Adapter<RVAdapter.NotificationViewHolder>{
 
@@ -234,14 +210,14 @@ public class AnnouncementsActivity extends AppCompatActivity
         }
 
         @Override
-        public RVAdapter.NotificationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public NotificationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_layout, parent, false);
-            RVAdapter.NotificationViewHolder nvh = new RVAdapter.NotificationViewHolder(v);
+            NotificationViewHolder nvh = new NotificationViewHolder(v);
             return nvh;
         }
 
         @Override
-        public void onBindViewHolder(RVAdapter.NotificationViewHolder holder, int position) {
+        public void onBindViewHolder(NotificationViewHolder holder, int position) {
             holder.author.setText(messages.get(position).getAuthor());
             holder.content.setText(messages.get(position).getContent());
             holder.channel.setText(messages.get(position).getChannel());
@@ -272,4 +248,13 @@ public class AnnouncementsActivity extends AppCompatActivity
             }
         }
     }
+
+
+
+
+
+
 }
+
+
+
--- app/src/main/java/edu/upenn/cis350/clubapp/MainActivity.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/MainActivity.java
@@ -2,7 +2,6 @@ package edu.upenn.cis350.clubapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Movie;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,8 +15,6 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -27,7 +24,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
