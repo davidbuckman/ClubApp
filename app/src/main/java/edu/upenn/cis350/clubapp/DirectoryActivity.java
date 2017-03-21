44d646830eb0cc7b0506dce5708bed8f4fc763ab
JennaB42 <jennabarton42@gmail.com>
2017-03-21 00:51:32 -0400

    updated xml and display to fix directory menu bar

--- app/src/main/java/edu/upenn/cis350/clubapp/DirectoryActivity.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/DirectoryActivity.java
@@ -65,7 +65,7 @@ public class DirectoryActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar); //TODO: figure out why this sits below
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
