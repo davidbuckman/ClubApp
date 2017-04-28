2c2128a7fb875b4ee6925a2167f7380592c8c023
Abhi <suriabhinav1997@gmail.com>
2017-04-28 06:17:51 -0400

    iteration done

--- app/src/main/java/edu/upenn/cis350/clubapp/CalendarEventActivity.java
+++ app/src/main/java/edu/upenn/cis350/clubapp/CalendarEventActivity.java
@@ -1,9 +1,13 @@
 package edu.upenn.cis350.clubapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,8 +31,9 @@ public class CalendarEventActivity extends AppCompatActivity {
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
     String clubID;
+    boolean isAdmin = false;
 
-
+    Context mContext;
     private SimpleDateFormat dateFormatForDisplaying = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", Locale.getDefault());
 
 
@@ -38,28 +43,43 @@ public class CalendarEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_event);
         EVENT_ID = getIntent().getStringExtra("EVENT_ID");
+        clubID = getIntent().getStringExtra("CLUB_ID");
+
+        mContext = this;
 
         final TextView title = (TextView) findViewById(R.id.title);
         final TextView date = (TextView) findViewById(R.id.date);
         final TextView author = (TextView) findViewById(R.id.author);
         final TextView description = (TextView) findViewById(R.id.description);
+        final Button editEvent = (Button) findViewById(R.id.edit_event);
+        final Button deleteEvent = (Button) findViewById(R.id.delete_event);
+
+        editEvent.setVisibility(View.GONE);
+        deleteEvent.setVisibility(View.GONE);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(Html.fromHtml("<font color='#ffffff'>Event</font>"));
         DatabaseReference ref = mDatabaseReference;
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                DataSnapshot ds = dataSnapshot.child("events").child(EVENT_ID);
-                ClubEvent clubEvent = new ClubEvent(ds.child("author").getValue(String.class),
-                        ds.child("description").getValue(String.class),
-                        ds.child("name").getValue(String.class),
-                        ds.child("date").getValue(Long.class),
-                        EVENT_ID);
-                title.setText(clubEvent.getName());
-                date.setText("WHEN: " + dateFormatForDisplaying.format(new Date(clubEvent.getDate())));
-                author.setText("CREATED BY: " + clubEvent.getAuthor());
-                description.setText("DESCRIPTION: \n" + clubEvent.getDescription());
+                try {
+                    DataSnapshot ds = dataSnapshot.child("events").child(EVENT_ID);
+                    ClubEvent clubEvent = new ClubEvent(ds.child("author").getValue(String.class),
+                            ds.child("description").getValue(String.class),
+                            ds.child("name").getValue(String.class),
+                            ds.child("date").getValue(Long.class),
+                            EVENT_ID);
+                    title.setText(clubEvent.getName());
+                    if (clubEvent.getDate() == null) {
+                        finish();
+                    }
+                    date.setText("WHEN: " + dateFormatForDisplaying.format(new Date(clubEvent.getDate())));
+                    author.setText("CREATED BY: " + clubEvent.getAuthor());
+                    description.setText("DESCRIPTION: \n" + clubEvent.getDescription());
+                } catch(Exception e) {
+                    finish();
+                }
 
             }
 
@@ -69,6 +89,47 @@ public class CalendarEventActivity extends AppCompatActivity {
             }
         });
 
+        ref.child("clubs").child(clubID).child("members").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("HIT THE ADMIN CODE");
+                isAdmin = dataSnapshot.child("isAdmin").getValue(Boolean.class);
+                if (isAdmin) {
+                    editEvent.setVisibility(View.VISIBLE);
+                    deleteEvent.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        System.out.println(isAdmin);
+
+            editEvent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent();
+                    i.putExtra("EVENT", EVENT_ID);
+                    i.putExtra("CLUB", clubID);
+                    i.setClass(mContext, AddEventActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            });
+
+            deleteEvent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatabaseReference ref = mDatabaseReference;
+                    ref.child("clubs").child(clubID).child("events").child(EVENT_ID).removeValue();
+                    ref.child("events").child(EVENT_ID).removeValue();
+                    finish();
+                }
+            });
 
 
     }
